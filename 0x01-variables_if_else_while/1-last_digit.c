1aaa83044881ad78bab446735503aa5c4b6c7137
moetezc <64417498+moetezc@users.noreply.github.com>
2020-06-11 13:23:23 +0100

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,6 +19,5 @@ else if (last < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 else if (last == 0)
 printf("Last digit of %d is %d and is 0\n", n, last);
-}
 return (0);
 }
