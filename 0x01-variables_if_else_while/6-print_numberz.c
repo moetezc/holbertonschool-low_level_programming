978c951049c10604e6875ba864332616ad56bd36
moetezc <moetezc@gmail.com>
2020-06-12 09:10:02 +0000

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the main function.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar("\n");
+	return (0);
+}
