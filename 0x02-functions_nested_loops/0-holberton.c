cfe587e1bc946dd3431d3457369a22c20c971942
moetezc <moetezc@gmail.com>
2020-06-15 10:51:25 +0000

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: On success 1.
+ */
+int main(void)
+{
+	_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
