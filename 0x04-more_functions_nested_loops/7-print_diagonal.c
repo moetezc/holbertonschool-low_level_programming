2e9b719b3ec87bed958977f0d85fa91e54702db4
moetezc <moetezc@gmail.com>
2020-06-18 14:06:49 +0000

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,10 +14,10 @@ void print_diagonal(int n)
 	}
 	for (i = 1; i <= n; i++)
 	{
-			for (j = 1; j < i; j++)
-			{
-				_putchar(32);
-			}
+		for (j = 1; j < i; j++)
+		{
+			_putchar(32);
+		}
 		_putchar('\\');
 		_putchar('\n');
 	}
