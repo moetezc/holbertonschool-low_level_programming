fe3326bf92472f8bd8837f66244b6dbddaef75b0
moetezc <moetezc@gmail.com>
2020-06-18 09:51:49 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - print numbers.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+_putchar(i);
+	}
+	_putchar('\n');
+}
