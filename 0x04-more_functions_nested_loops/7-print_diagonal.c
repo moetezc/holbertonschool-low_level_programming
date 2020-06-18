399e07505bd34c3e05feef61863cf97f136682a9
moetezc <moetezc@gmail.com>
2020-06-18 13:59:33 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_diagonal - print diagonal with space.
+ *
+ * @n: paramater.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(32);
+			}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
