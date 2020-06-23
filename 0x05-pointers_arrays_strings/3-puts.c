a56ec0d7620363b9d2c7c545438694c8cd3d3707
moetezc <moetezc@gmail.com>
2020-06-23 19:58:32 +0000

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string, followed by a new line.
+ *
+ * @str : pointer.
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar (*str);
+		str++;
+	}
+	_putchar ('\n');
+}
